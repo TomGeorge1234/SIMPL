620bcd1a6a222ffcb6cac61fa18d40ff430cf828
Andrew Landau <andrewtylerlandau@gmail.com>
2026-02-17 12:04:54 +0000

    Force add init bc gitignore :)

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+"""
+Tests for SIMPL package: 
+- this folder includes tests for the feature/trial-boundaries branch PR
+- validate_trial_boundaries.py tests the trial boundaries functionality of SIMPL
+  by using the demo data with and without T trial boundaries. It makes two plots 
+  to show how the results differ. 
+"""
