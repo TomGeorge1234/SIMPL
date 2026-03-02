b6da8940aad3ae319a7c2bbb82d6a9dec38446ce
Tom George <tomgeorge1@btinternet.com>
2026-03-02 09:39:03 -0500

    fix tests

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -8,13 +8,13 @@ from simpl.environment import Environment
 class TestEnvironment1D:
     def test_correct_dimensions(self):
         X = np.random.randn(100, 1)
-        env = Environment(X, verbose=False)
+        env = Environment(X)
         assert env.D == 1
         assert env.dim == ["x"]
 
     def test_correct_shape(self):
         X = np.random.randn(100, 1)
-        env = Environment(X, bin_size=0.1, verbose=False)
+        env = Environment(X, bin_size=0.1)
         assert len(env.discrete_env_shape) == 1
         assert env.flattened_discretised_coords.shape[1] == 1
 
@@ -22,13 +22,13 @@ class TestEnvironment1D:
 class TestEnvironment2D:
     def test_correct_dimensions(self):
         X = np.random.randn(100, 2)
-        env = Environment(X, verbose=False)
+        env = Environment(X)
         assert env.D == 2
         assert env.dim == ["x", "y"]
 
     def test_coords(self):
         X = np.random.randn(100, 2)
-        env = Environment(X, verbose=False)
+        env = Environment(X)
         assert "x" in env.coords_dict
         assert "y" in env.coords_dict
 
@@ -36,7 +36,7 @@ class TestEnvironment2D:
 class TestEnvironment3D:
     def test_correct_dimensions(self):
         X = np.random.randn(100, 3)
-        env = Environment(X, verbose=False)
+        env = Environment(X)
         assert env.D == 3
         assert env.dim == ["x", "y", "z"]
 
@@ -44,7 +44,7 @@ class TestEnvironment3D:
 class TestEnvironmentPadding:
     def test_limits_extend_beyond_data(self):
         X = np.array([[0.0, 0.0], [1.0, 1.0]])
-        env = Environment(X, pad=0.1, verbose=False)
+        env = Environment(X, pad=0.1)
         assert env.lims[0][0] < 0.0
         assert env.lims[1][0] > 1.0
 
@@ -52,7 +52,7 @@ class TestEnvironmentPadding:
 class TestEnvironmentBinSize:
     def test_bin_count_matches_expected(self):
         X = np.array([[0.0], [1.0]])
-        env = Environment(X, pad=0.0, bin_size=0.1, verbose=False)
+        env = Environment(X, pad=0.0, bin_size=0.1)
         n_bins = env.discrete_env_shape[0]
         expected = int(np.ceil((1.0 - 0.0) / 0.1))
         assert abs(n_bins - expected) <= 1
@@ -62,14 +62,14 @@ class TestEnvironmentForceLims:
     def test_overrides_data_limits(self):
         X = np.random.randn(100, 2)
         lims = ((-5.0, -5.0), (5.0, 5.0))
-        env = Environment(X, force_lims=lims, verbose=False)
+        env = Environment(X, force_lims=lims)
         assert env.lims == lims
 
 
 class TestEnvironmentDiscretisedCoords:
     def test_mesh_grid_correct(self):
         X = np.random.randn(50, 2)
-        env = Environment(X, verbose=False)
+        env = Environment(X)
         assert env.discretised_coords.shape[0] == 2
         assert env.discretised_coords.shape[1:] == env.discrete_env_shape
 
@@ -77,7 +77,7 @@ class TestEnvironmentDiscretisedCoords:
 class TestEnvironmentFlattenedCoords:
     def test_shape(self):
         X = np.random.randn(50, 2)
-        env = Environment(X, verbose=False)
+        env = Environment(X)
         n_bins = np.prod(env.discrete_env_shape)
         assert env.flattened_discretised_coords.shape == (n_bins, 2)
 
@@ -88,6 +88,6 @@ class TestEnvironmentPlot:
 
         matplotlib.use("Agg")
         X = np.random.randn(50, 2)
-        env = Environment(X, verbose=False)
+        env = Environment(X)
         ax = env.plot_environment()
         assert ax is not None
