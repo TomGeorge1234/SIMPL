cfc874f05f7b6c1a2a5a777af9e8df8a9bcd1558
Tom George <tomgeorge1@btinternet.com>
2026-03-31 18:19:14 -0400

    increase number of simultaneous runs

--- sweeps/sweep_placecells.sh
+++ sweeps/sweep_placecells.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=simpl-sweep-pc
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=4G
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
 #SBATCH --time=24:00:00
 #SBATCH --output=/dev/null
 
